f2000fa0bdea14da45eba1b9ddeb3c59aef35ce1
Christopher Wróbel <wrobel1999@gmail.com>
2025-11-23 01:28:56 +0100

    build: add nix build

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "3bir project flake";
+  description = "threebir project flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,6 +16,49 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        threebir = pkgs.python312Packages.buildPythonPackage {
+          pname = "threebir";
+          version = "0.1.0";
+          pyproject = true;
+          src = ./.;
+          build-system = with pkgs.python312Packages; [ setuptools ];
+          dependencies = with pkgs.python312Packages; [
+            flask
+            flask-socketio
+            waitress
+          ];
+        };
+        myApp = pkgs.python312Packages.buildPythonApplication {
+          pname = "flask-nix-server";
+          version = "0.1.0";
+          src = ./.;
+          format = "pyproject";
+
+          # Build dependencies
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+            pkgs.python312Packages.setuptools
+            pkgs.python312Packages.wheel
+          ];
+
+          # Runtime dependencies
+          propagatedBuildInputs = [
+            pkgs.python312Packages.flask
+            pkgs.python312Packages.waitress
+          ];
+
+          # This is the magic step.
+          # 1. We look for the 'waitress-serve' binary provided by the waitress package.
+          # 2. We create a new binary in $out/bin named 'start-server'.
+          # 3. We wrap it to execute waitress-serve with specific arguments.
+          # 4. We add the current folder to PYTHONPATH so waitress can find 'app.py'
+          postInstall = ''
+            makeWrapper ${pkgs.python312Packages.waitress}/bin/waitress-serve $out/bin/start-server \
+                --prefix PYTHONPATH : "$out/lib/${pkgs.python312.libPrefix}/site-packages" \
+                --add-flags "--port=8080" \
+                --add-flags "app:app"
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -24,11 +67,17 @@
               pp: with pp; [
                 flask
                 flask-socketio
+                build
+                waitress
+                threebir
               ]
             ))
+            threebir
             pyright
+            myApp
           ];
         };
+        packages.default = myApp;
       }
     );
 }
